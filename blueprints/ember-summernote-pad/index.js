735d09566c3cb93f70fb7e7b8512773491f23b43
maiyaporn <p.maiyaporn@gmail.com>
2016-06-13 18:09:03 -0500

    Updated default blueprint

--- blueprints/ember-summernote-pad/index.js
+++ blueprints/ember-summernote-pad/index.js
@@ -1,8 +1,17 @@
 /*jshint node:true*/
 module.exports = {
-  description: '',
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   afterInstall: function(options) {
-    // return this.addBowerPackageToProject('ember-summernote-pad');
+    return this.addBowerPackagesToProject([
+      { name: 'jquery',                 target: '~2.2.4' },
+      { name: 'bootstrap',              target: '~3.3.6' },
+      { name: 'font-awesome',           target: '~4.6.3' },
+      { name: 'summernote',             target: '~0.8.1' }
+    ]);
   }
 };
